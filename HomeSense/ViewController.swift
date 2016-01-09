f03733663f5a86c6e8af54f560b534e016d491b5
Dorian Nowak <01miru@gmail.com>
2016-01-09 20:52:44 +0100

    Initial Commit

---
+++ HomeSense/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HomeSense
+//
+//  Created by Dorian Nowak on 09.01.2016.
+//  Copyright © 2016 Dorian Nowak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
